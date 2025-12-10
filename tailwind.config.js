c94384b8f9fd97b7a9715374aa5ea2b5ddcaaf3b
Rasim Matsko <rasimmatsko0@gmail.com>
2025-12-10 04:22:21 +0200

    Add project files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ tailwind.config = {
         container: {
             center: true,  
             screens: {
-                DEFAULT: '1160px',  
+                DEFAULT: '1240px',  
             },
         },
         extend: {
@@ -11,7 +11,7 @@ tailwind.config = {
                 'red-800': '#960001',
                 'red-900': '#760203',
                 'red-950': '#490000',
-
+                'grey-50': '#EBEBEB',
                 'grey-100': '#B9B9B9',
                 'grey-200': '#CECECE',
                 'grey-250': '#393939',
@@ -27,6 +27,7 @@ tailwind.config = {
                 'accordion3-bg': "url('./assets/images/accordion/full-bg3.png')",
                 'gradient-grey': 'linear-gradient(90deg, #2A2A2A 0%, #FFFFFF 100%)',
                 'footer-bg-gradient': 'linear-gradient(356deg, #030303 25%, rgba(3, 3, 3, 0.00) 110%);',
+                'linear-65': 'linear-gradient(90deg, rgb(0 0 0 / 70%) 0%, transparent 210%)',
             },
             maxWidth: {
                 'container': '1160px', 
